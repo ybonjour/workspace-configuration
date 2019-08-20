27c3d153b01c47a2b2f906395e9373521a3dca1d
Yves Bonjour <yves.bonjour@gmail.com>
2019-08-20 20:33:01 +0200

    Adds comment about .zshrc revert

--- scripts/setup/install-oh-my-zsh.sh
+++ scripts/setup/install-oh-my-zsh.sh
@@ -11,6 +11,8 @@ curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools
 git clone https://github.com/bhilburn/powerlevel9k.git "${HOME}/.oh-my-zsh/custom/themes/powerlevel9k" || true
 git clone https://github.com/zsh-users/zsh-autosuggestions "${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions" || true
 chsh -s /bin/zsh
+
+# Oh My ZSH overrides .zshrc. We need to revert this
 pushd "${HOME}"
 	git checkout -- .zshrc
 popd
